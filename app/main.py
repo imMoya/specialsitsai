3be089847065355988ed640de344363618e894ca
Ignacio Moya <imoyared@gmail.com>
2024-11-16 11:16:02 +0100

    feat(app): add .env read to main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,20 @@ from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 import json
 from typing import Dict
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os 
+import structlog
+
+logger = structlog.get_logger(__name__)
+
+root_dir = Path(__file__).parent.parent
+env_path = root_dir / '.env'
+load_dotenv(dotenv_path=env_path)
+
+BASE_PATH = os.getenv("BASE_PATH")
+if not BASE_PATH:
+    raise ValueError("BASE_PATH not found in environment variables")
 
 app = FastAPI(title="SpecialSitsAI API")
 
@@ -35,7 +49,7 @@ def read_json_mapper(directory: Path) -> Dict:
 @app.get("/")
 async def root():
     """Get summary of available tickers from both oddlots and spinoffs"""
-    base_path = Path("/Users/ignaciomoyaredondo/Library/CloudStorage/OneDrive-Personal/obsidian/01_projects/secedgarspecial/")
+    base_path = Path(BASE_PATH)
     
     return {
         "oddlots": read_json_mapper(base_path / "db_oddlots"),
@@ -45,7 +59,7 @@ async def root():
 @app.get("/details/{dataset}/{ticker}")
 async def get_ticker_details(dataset: str, ticker: str):
     """Get specific details for a ticker from either oddlots or spinoffs"""
-    base_path = Path("/Users/ignaciomoyaredondo/OneDrive/obsidian/01_projects/secedgarspecial")
+    base_path = Path(BASE_PATH)
     
     if dataset not in ["oddlots", "spinoffs"]:
         raise HTTPException(status_code=400, detail="Dataset must be either 'oddlots' or 'spinoffs'")
